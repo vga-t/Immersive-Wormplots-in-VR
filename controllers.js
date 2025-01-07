dd7aded21218964569c010a72171612ec3355c51
vipin <georgevipin1@gmail.com>
2025-01-07 14:12:34 +0100

    Making code modular and less redundant.

--- controllers.js
+++ controllers.js
@@ -1,136 +1,144 @@
+import { groupMeshes,toggleVisibility, getRandomColor } from './helpers.js';
 
-import { groupMeshes, toggleVisibility, getRandomColor } from './helpers.js';
+export async function setupControllers(scene, xrHelper, panel, anchor, ground) {
+    let leftController, rightController;
+    let initialDistance = null;
+    let initialScale = null;
+    let isScaling = false;
+    let pickedMesh = null;
+    let originalParent = null;
+    let isPanelVisible = false;
+    let isFlying = false;
+    let flightSpeed = 0.1;
 
-export function setupControllers(scene) {
-    const xrHelper = scene.createDefaultXRExperienceAsync({
-        floorMeshes: [ground],
-        inputOptions: {
-            disableDefaultControllerMesh: true,
+    function startScaling() {
+        if (leftController && rightController && pickedMesh) {
+            const leftPosition = leftController.grip.position;
+            const rightPosition = rightController.grip.position;
+            initialDistance = BABYLON.Vector3.Distance(leftPosition, rightPosition);
+            initialScale = pickedMesh.scaling.clone();
+            isScaling = true;
+        }
+    }
+
+    function stopScaling() {
+        isScaling = false;
+        initialDistance = null;
+        initialScale = null;
+    }
+
+    function updatePanelPosition() {
+        const xrCamera = xrHelper.baseExperience.camera;
+        const forward = xrCamera.getDirection(BABYLON.Vector3.Forward());
+        anchor.position = xrCamera.position.add(forward.scale(2));
+        anchor.lookAt(xrCamera.position, 0, Math.PI, Math.PI);
+    }
+
+    scene.onBeforeRenderObservable.add(() => {
+        if (isScaling && leftController && rightController && pickedMesh) {
+            const leftPosition = leftController.grip.position;
+            const rightPosition = rightController.grip.position;
+            const currentDistance = BABYLON.Vector3.Distance(leftPosition, rightPosition);
+            if (initialDistance && initialScale) {
+                const scaleFactor = currentDistance / initialDistance;
+                pickedMesh.scaling = initialScale.multiply(new BABYLON.Vector3(scaleFactor, scaleFactor, scaleFactor));
+            }
+        }
+
+        if (isFlying) {
+            const xrCamera = xrHelper.baseExperience.camera;
+            const forward = xrCamera.getDirection(BABYLON.Vector3.Forward()).scale(flightSpeed);
+            xrCamera.position.addInPlace(forward);
         }
     });
 
-    xrHelper.then(async (xrHelperInstance) => {
-        let leftController, rightController;
-        let initialDistance = null;
-        let initialScale = null;
-        let isScaling = false;
-        let pickedMesh = null;
-        let originalParent = null;
+    xrHelper.input.onControllerAddedObservable.add((controller) => {
+        controller.onMotionControllerInitObservable.add((motionController) => {
+            const triggerComponent = motionController.getComponent('xr-standard-trigger');
+            const squeezeComponent = motionController.getComponent('xr-standard-squeeze');
+            const menuComponent = motionController.getComponent('x-button');
+            const flightComponent = motionController.getComponent('y-button');
 
-        // Existing scaling and other methods remain the same
-        function startScaling() {
-            if (leftController && rightController && pickedMesh) {
-                const leftPosition = leftController.grip.position;
-                const rightPosition = rightController.grip.position;
-                initialDistance = BABYLON.Vector3.Distance(leftPosition, rightPosition);
-                initialScale = pickedMesh.scaling.clone();
-                isScaling = true;
-            }
-        }
+            const profileId = motionController.profileId;
+            BABYLON.SceneLoader.ImportMesh("", "https://controllers.babylonjs.com/", `${profileId}.glb`, scene, (meshes) => {
+                meshes.forEach(mesh => {
+                    mesh.parent = motionController.rootMesh;
+                    const emissiveMaterial = new BABYLON.StandardMaterial("emissiveMaterial", scene);
+                    emissiveMaterial.emissiveColor = new BABYLON.Color3(1, 1, 1);
+                    mesh.material = emissiveMaterial;
+                });
+            });
 
-        function stopScaling() {
-            isScaling = false;
-            initialDistance = null;
-            initialScale = null;
-        }
+            if (motionController.handness === 'left') {
+                leftController = controller;
 
-        scene.onBeforeRenderObservable.add(() => {
-            if (isScaling && leftController && rightController && pickedMesh) {
-                const leftPosition = leftController.grip.position;
-                const rightPosition = rightController.grip.position;
-                const currentDistance = BABYLON.Vector3.Distance(leftPosition, rightPosition);
-                if (initialDistance && initialScale) {
-                    const scaleFactor = currentDistance / initialDistance;
-                    pickedMesh.scaling = initialScale.multiply(new BABYLON.Vector3(scaleFactor, scaleFactor, scaleFactor));
-                }
-            }
-        });
-
-        xrHelperInstance.input.onControllerAddedObservable.add((controller) => {
-            controller.onMotionControllerInitObservable.add((motionController) => {
-                const triggerComponent = motionController.getComponent('xr-standard-trigger');
-                const squeezeComponent = motionController.getComponent('xr-standard-squeeze');
-                const menuComponent = motionController.getComponent('x-button');
-
-                // Load the appropriate controller model based on profileId
-                const profileId = motionController.profileId;
-                BABYLON.SceneLoader.ImportMesh("", "https://controllers.babylonjs.com/", `${profileId}.glb`, scene, (meshes) => {
-                    meshes.forEach(mesh => {
-                        mesh.parent = motionController.rootMesh;
-
-                        // Make the mesh emissive
-                        const emissiveMaterial = new BABYLON.StandardMaterial("emissiveMaterial", scene);
-                        emissiveMaterial.emissiveColor = new BABYLON.Color3(1, 1, 1); // Set emissive color to white
-                        mesh.material = emissiveMaterial;
+                if (menuComponent) {
+                    menuComponent.onButtonStateChangedObservable.add(() => {
+                        if (menuComponent.pressed) {
+                            isPanelVisible = !isPanelVisible;
+                            if (isPanelVisible) {
+                                updatePanelPosition();
+                            }
+                            panel.isVisible = isPanelVisible;
+                            panel.children.forEach(button => {
+                                button.isVisible = isPanelVisible;
+                                button.isPickable = isPanelVisible;
+                            });
+                        }
                     });
+                }
+
+                if (flightComponent) {
+                    flightComponent.onButtonStateChangedObservable.add(() => {
+                        if (flightComponent.pressed) {
+                            isFlying = !isFlying;
+                        }
+                    });
+                }
+
+                squeezeComponent.onButtonStateChangedObservable.add(() => {
+                    if (squeezeComponent.changes.pressed) {
+                        if (squeezeComponent.pressed && leftController && rightController && pickedMesh) {
+                            startScaling();
+                        } else {
+                            stopScaling();
+                        }
+                    }
+                });
+            } else if (motionController.handness === 'right') {
+                rightController = controller;
+
+                triggerComponent.onButtonStateChangedObservable.add(() => {
+                    if (triggerComponent.changes.pressed) {
+                        if (triggerComponent.pressed) {
+                            let mesh = scene.meshUnderPointer;
+                            if (xrHelper.pointerSelection.getMeshUnderPointer) {
+                                mesh = xrHelper.pointerSelection.getMeshUnderPointer(controller.uniqueId);
+                            }
+                            if (mesh === ground) {
+                                return;
+                            }
+                            const group = Object.keys(groupMeshes).find(group =>
+                                groupMeshes[group].parentNode === mesh ||
+                                groupMeshes[group].LineSystem === mesh ||
+                                groupMeshes[group].ribbon === mesh
+                            );
+                            if (group) {
+                                pickedMesh = groupMeshes[group].parentNode;
+                                originalParent = pickedMesh.parent;
+                                pickedMesh.setParent(motionController.rootMesh);
+                            }
+                        } else {
+                            if (pickedMesh) {
+                                pickedMesh.setParent(originalParent);
+                                pickedMesh = null;
+                            }
+                        }
+                    }
                 });
 
-                if (motionController.handness === 'left') {
-                    leftController = controller;
 
-                    // Menu button toggle
-                    if (menuComponent) {
-                        menuComponent.onButtonStateChangedObservable.add(() => {
-                            if (menuComponent.pressed) {
-                                isPanelVisible = !isPanelVisible;
-                                if (isPanelVisible) {
-                                    updatePanelPosition();
-                                }
-                                panel.isVisible = isPanelVisible;
-                                panel.children.forEach(button => {
-                                    button.isVisible = isPanelVisible;
-                                    button.isPickable = isPanelVisible;
-                                });
-                            }
-                        });
-                    }
-
-                    // Scaling using left squeeze and right trigger
-                    squeezeComponent.onButtonStateChangedObservable.add(() => {
-                        if (squeezeComponent.changes.pressed) {
-                            if (squeezeComponent.pressed && leftController && rightController && pickedMesh) {
-                                startScaling();
-                            } else {
-                                stopScaling();
-                            }
-                        }
-                    });
-                } else if (motionController.handness === 'right') {
-                    rightController = controller;
-
-                    // Move worm plots using either controller's trigger
-                    triggerComponent.onButtonStateChangedObservable.add(() => {
-                        if (triggerComponent.changes.pressed) {
-                            if (triggerComponent.pressed) {
-                                let mesh = scene.meshUnderPointer;
-                                if (xrHelperInstance.pointerSelection.getMeshUnderPointer) {
-                                    mesh = xrHelperInstance.pointerSelection.getMeshUnderPointer(controller.uniqueId);
-                                }
-                                if (mesh === ground) {
-                                    return;
-                                }
-                                const group = Object.keys(groupMeshes).find(group =>
-                                    groupMeshes[group].parentNode === mesh ||
-                                    groupMeshes[group].LineSystem === mesh ||
-                                    groupMeshes[group].ribbon === mesh
-                                );
-                                if (group) {
-                                    pickedMesh = groupMeshes[group].parentNode;
-                                    originalParent = pickedMesh.parent;
-                                    pickedMesh.setParent(motionController.rootMesh);
-                                }
-                            } else {
-                                if (pickedMesh) {
-                                    pickedMesh.setParent(originalParent);
-                                    pickedMesh = null;
-                                }
-                            }
-                        }
-                    });
-                }
-            });
+            }
         });
-
-        // ...existing controller-related code...
     });
 }
--- scene.js
+++ scene.js
@@ -2,6 +2,10 @@ import { loadCSVData } from './dataLoader.js';
 import { currentDataset, attribute1, attribute2, setupUI } from './ui.js'; // Add setupUI import
 import { datasetConfig } from './config.js';
 import { setupControllers } from './controllers.js';
+import {groupMeshes} from './helpers.js'; 
+
+
+
 
 export async function initializeScene() {
     try {
@@ -31,14 +35,6 @@ export async function initializeScene() {
             }
         });
 
-        let leftController, rightController;
-        let initialDistance = null;
-        let initialScale = null;
-        let isScaling = false;
-        let pickedMesh = null;
-        let originalParent = null;
-        const groupMeshes = {};
-
         // Create the 3D UI manager
         const manager = new BABYLON.GUI.GUI3DManager(scene);
 
@@ -53,6 +49,9 @@ export async function initializeScene() {
         // Create an anchor for the panel
         const anchor = new BABYLON.TransformNode("panelAnchor");
         panel.linkToTransformNode(anchor);
+
+        // Call setupControllers to handle all controller logic
+        await setupControllers(scene, xrHelper, panel, anchor, ground);
 
         // Create walls around the ground
         const wallMaterial = new BABYLON.StandardMaterial("wallMaterial", scene);
@@ -250,158 +249,6 @@ export async function initializeScene() {
 
         Groups.forEach(group => {
             addButton(group, colors[group]);
-        });
-
-        // Function to update panel position
-        const updatePanelPosition = () => {
-            const xrCamera = xrHelper.baseExperience.camera;
-            const forward = xrCamera.getDirection(BABYLON.Vector3.Forward());
-            anchor.position = xrCamera.position.add(forward.scale(2));
-            anchor.lookAt(xrCamera.position, 0, Math.PI, Math.PI);
-        };
-
-        // Toggle panel visibility and interactivity
-        let isPanelVisible = false;
-
-        xrHelper.input.onControllerAddedObservable.add((controller) => {
-            controller.onMotionControllerInitObservable.add((motionController) => {
-                const triggerComponent = motionController.getComponent('xr-standard-trigger');
-                const squeezeComponent = motionController.getComponent('xr-standard-squeeze');
-                const menuComponent = motionController.getComponent('x-button');
-
-                // Load the appropriate controller model based on profileId
-                const profileId = motionController.profileId;
-                BABYLON.SceneLoader.ImportMesh("", "https://controllers.babylonjs.com/", `${profileId}.glb`, scene, (meshes) => {
-                    meshes.forEach(mesh => {
-                        mesh.parent = motionController.rootMesh;
-
-                        // Make the mesh emissive
-                        const emissiveMaterial = new BABYLON.StandardMaterial("emissiveMaterial", scene);
-                        emissiveMaterial.emissiveColor = new BABYLON.Color3(1, 1, 1); // Set emissive color to white
-                        mesh.material = emissiveMaterial;
-                    });
-                });
-
-                if (motionController.handness === 'left') {
-                    leftController = controller;
-
-                    // Menu button toggle
-                    if (menuComponent) {
-                        menuComponent.onButtonStateChangedObservable.add(() => {
-                            if (menuComponent.pressed) {
-                                isPanelVisible = !isPanelVisible;
-                                if (isPanelVisible) {
-                                    updatePanelPosition();
-                                }
-                                panel.isVisible = isPanelVisible;
-                                panel.children.forEach(button => {
-                                    button.isVisible = isPanelVisible;
-                                    button.isPickable = isPanelVisible;
-                                });
-                            }
-                        });
-                    }
-
-                    // Scaling using left squeeze and right trigger
-                    squeezeComponent.onButtonStateChangedObservable.add(() => {
-                        if (squeezeComponent.changes.pressed) {
-                            if (squeezeComponent.pressed && leftController && rightController && pickedMesh) {
-                                startScaling();
-                            } else {
-                                stopScaling();
-                            }
-                        }
-                    });
-                } else if (motionController.handness === 'right') {
-                    rightController = controller;
-
-                    // Move worm plots using either controller's trigger
-                    triggerComponent.onButtonStateChangedObservable.add(() => {
-                        if (triggerComponent.changes.pressed) {
-                            if (triggerComponent.pressed) {
-                                let mesh = scene.meshUnderPointer;
-                                if (xrHelper.pointerSelection.getMeshUnderPointer) {
-                                    mesh = xrHelper.pointerSelection.getMeshUnderPointer(controller.uniqueId);
-                                }
-                                if (mesh === ground) {
-                                    return;
-                                }
-                                const group = Object.keys(groupMeshes).find(group =>
-                                    groupMeshes[group].parentNode === mesh ||
-                                    groupMeshes[group].LineSystem === mesh ||
-                                    groupMeshes[group].ribbon === mesh
-                                );
-                                if (group) {
-                                    pickedMesh = groupMeshes[group].parentNode;
-                                    originalParent = pickedMesh.parent;
-                                    pickedMesh.setParent(motionController.rootMesh);
-                                }
-                            } else {
-                                if (pickedMesh) {
-                                    pickedMesh.setParent(originalParent);
-                                    pickedMesh = null;
-                                }
-                            }
-                        }
-                    });
-                }
-            });
-        });
-
-        // Existing scaling and other methods remain the same
-        function startScaling() {
-            if (leftController && rightController && pickedMesh) {
-                const leftPosition = leftController.grip.position;
-                const rightPosition = rightController.grip.position;
-                initialDistance = BABYLON.Vector3.Distance(leftPosition, rightPosition);
-                initialScale = pickedMesh.scaling.clone();
-                isScaling = true;
-            }
-        }
-
-        function stopScaling() {
-            isScaling = false;
-            initialDistance = null;
-            initialScale = null;
-        }
-
-        scene.onBeforeRenderObservable.add(() => {
-            if (isScaling && leftController && rightController && pickedMesh) {
-                const leftPosition = leftController.grip.position;
-                const rightPosition = rightController.grip.position;
-                const currentDistance = BABYLON.Vector3.Distance(leftPosition, rightPosition);
-                if (initialDistance && initialScale) {
-                    const scaleFactor = currentDistance / initialDistance;
-                    pickedMesh.scaling = initialScale.multiply(new BABYLON.Vector3(scaleFactor, scaleFactor, scaleFactor));
-                }
-            }
-        });
-
-        let isFlying = false; // Tracks if flying mode is active
-        let flightSpeed = 0.1; // Base flight speed
-
-        xrHelper.input.onControllerAddedObservable.add((controller) => {
-            controller.onMotionControllerInitObservable.add((motionController) => {
-                const joystickComponent = motionController.getComponent('xr-standard-thumbstick');
-
-                const toggleButton = motionController.getComponent('y-button');
-                if (toggleButton) {
-                    toggleButton.onButtonStateChangedObservable.add(() => {
-                        if (toggleButton.pressed) {
-                            isFlying = !isFlying;
-                        }
-                    });
-                }
-            });
-        });
-
-        // Update camera position during flight
-        scene.onBeforeRenderObservable.add(() => {
-            if (isFlying) {
-                const xrCamera = xrHelper.baseExperience.camera;
-                const forward = xrCamera.getDirection(BABYLON.Vector3.Forward()).scale(flightSpeed);
-                xrCamera.position.addInPlace(forward);
-            }
         });
 
         engine.runRenderLoop(function() {
